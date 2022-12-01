374cefe4370862bbb5dd26964dd4bdee515eea1d
akashvishwakarma27 <akashvish4masai@gmail.com>
2022-12-01 22:50:56 +0530

    check code

---
+++ palindrome.js
@@ -0,0 +1,13 @@
+Q2. Check givin string is palindrome or Notification;
+
+str = "naman";
+new_str = "";
+
+for (let i = str.length-1; i=0; i--){
+new_str+=str[i];
+}
+if (str == new_str){
+    console.log("palindrome";)
+}else {
+    console.log("not");
+}
---
+++ prime.js
@@ -0,0 +1,15 @@
+Q1. Check the number is prime or Not
+
+num = 13
+
+let count = 0; 
+for(let i = 0; i<=num; i++):
+if (num%i==0){
+    count++;
+}
+
+if (count == 0){
+    console.log("number is prime ");
+}else {
+    console.log("number is not prime");
+}
